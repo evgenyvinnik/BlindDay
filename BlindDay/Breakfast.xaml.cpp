842a25b4ed227f97eb64a3c8e407ffc2865369ea
Mason Duan <mason.d.97@gmail.com>
2015-11-14 16:02:03 -0800

    Fixed the menue page. Started working on breakfast

---
+++ BlindDay/Breakfast.xaml.cpp
@@ -0,0 +1,33 @@
+﻿//
+// Breakfast.xaml.cpp
+// Implementation of the Breakfast class
+//
+
+#include "pch.h"
+#include "Breakfast.xaml.h"
+
+using namespace BlindDay;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+Breakfast::Breakfast()
+{
+	InitializeComponent();
+}
+
+
+void BlindDay::Breakfast::Grid_Loaded(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+
+}
